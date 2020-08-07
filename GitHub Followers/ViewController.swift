6f8c337fb8706a8896cd1452d13702b90905fffd
Vladimir Drozdin <rindzorcoop@gmail.com>
2020-08-07 18:14:45 +0300

    Initial Commit

---
+++ GitHub Followers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Followers
+//
+//  Created by  mac on 07.08.2020.
+//  Copyright © 2020 Vladimir Drozdin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
